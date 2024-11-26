98334e37ae84418b0d55e3d722e39159b2147b0a
Abdel-had HANAMI <101605739+Abdel-had@users.noreply.github.com>
2024-11-26 22:51:59 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Container, Button } from '@mui/material';
 import PipelineList from './components/PipelineList';
 import Monitoring from './components/Monitoring';
 
 function App() {
+  const [view, setView] = useState('pipelines');
+
   return (
     <div>
-      <h1>InfraEase</h1>
-      <PipelineList />
-      <Monitoring />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            InfraEase
+          </Typography>
+          <Button color="inherit" onClick={() => setView('pipelines')}>
+            Pipelines
+          </Button>
+          <Button color="inherit" onClick={() => setView('monitoring')}>
+            Monitoring
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container style={{ marginTop: '20px' }}>
+        {view === 'pipelines' && <PipelineList />}
+        {view === 'monitoring' && <Monitoring />}
+      </Container>
     </div>
   );
 }
