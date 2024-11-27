d92b004c0d56ec13e77fc4fb855046863fa620e7
Abdel-had HANAMI <101605739+Abdel-had@users.noreply.github.com>
2024-11-27 01:01:51 +0100

    Update views.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,18 +4,15 @@ from rest_framework.response import Response
 from .models import Pipeline
 from .serializers import PipelineSerializer
 
-# Vue pour gérer les pipelines (CRUD)
 class PipelineViewSet(viewsets.ModelViewSet):
     queryset = Pipeline.objects.all()
     serializer_class = PipelineSerializer
 
-# Vue pour fournir les données de monitoring
 class MonitoringView(APIView):
     def get(self, request):
-        # Données simulées pour le monitoring
         data = {
-            "cpu_usage": 50,  # % d'utilisation du CPU
-            "memory_usage": 2.5,  # Utilisation de la mémoire en GB
+            "cpu_usage": 50,  # Exemple de données simulées
+            "memory_usage": 2.5,  # Utilisation mémoire en GB
             "active_pipelines": 5,  # Nombre de pipelines actifs
         }
         return Response(data)
